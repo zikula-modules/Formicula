4afb60b63499316737ae2e5b0c94bfbaa0a3bca2
landseer <landseer@2cecb711-b51b-0410-93e8-fb3f36be984d>
2006-10-07 14:56:16 +0000

    updated changelog version raised to 1.1
    git-svn-id: https://code.zikula.org/svn/formicula/trunk@64 2cecb711-b51b-0410-93e8-fb3f36be984d

--- formicula/pnversion.php
+++ formicula/pnversion.php
@@ -1,7 +1,7 @@
 <?php
 // $Id$
 $modversion['name'] = 'formicula';
-$modversion['version'] = '1.0';
+$modversion['version'] = '1.1';
 $modversion['description'] = _FOR_DESC;
 $modversion['credits'] = 'pndocs/credits.txt';
 $modversion['help'] = 'pndocs/eng/manual.htm';
