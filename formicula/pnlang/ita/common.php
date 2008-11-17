5b4d930e42639f9b3913aa5bc54d9abceeed38f2
Landseer <Landseer@2cecb711-b51b-0410-93e8-fb3f36be984d>
2008-11-17 20:36:56 +0000

    added italian lang files, credits to Arthens!
    git-svn-id: https://code.zikula.org/svn/formicula/trunk@121 2cecb711-b51b-0410-93e8-fb3f36be984d

---
+++ formicula/pnlang/ita/common.php
@@ -0,0 +1,221 @@
+<?php
+// $Id$
+// ----------------------------------------------------------------------
+// LICENSE
+//
+// This program is free software; you can redistribute it and/or
+// modify it under the terms of the GNU General Public License (GPL)
+// as published by the Free Software Foundation; either version 2
+// of the License, or (at your option) any later version.
+//
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// To read the license please visit http://www.gnu.org/copyleft/gpl.html
+// ----------------------------------------------------------------------
+/**
+ * @copyright (C) Frank Schummertz
+ * @link http://code.zikula.org/projects/community-italian/
+ * @translated by Arthens
+ */
+
+define('_FOR_ACTIVATESPAMCHECK', 'Attiva spamcheck<br />(assicurati che i requisiti siano soddisfatti, leggi la documentazione per informazioni. Questa opzione sarà automaticamente disattivata se la funzione PHP per creare immagini non fosse disponibile)');
+define('_FOR_ADDCONTACT','Aggiungi contatto' );
+define('_FOR_ADMINMAIL1', 'Un visitatore del tuo sito ha usato il form per contattarti e mandarti ciò che segue:' );
+define('_FOR_ADMINMAIL2', 'L\'utente aveva i seguenti indirizzo IP/hostname: ' );
+define('_FOR_ADVICE_EMAIL', 'Per favore inserisci un indirizzo e-mail valido, come user@example.com.');
+define('_FOR_ADVICE_MANDATORY', 'Questo campo è obbligatorio.');
+define('_FOR_ADVICE_URL', 'Per favore inserisci un indirizzo internet valido come http://www.example.com.');
+define('_FOR_ALTERTABLEFAILED', 'impossibile modificare la tabella');
+
+//
+// B
+//
+define('_FOR_BACK', 'Torna al Contact Form' );
+define('_FOR_BADAUTHKEY', 'AuthKey Invalida');
+
+//
+// C
+//
+define('_FOR_CACHEDIRPROBLEM', 'la cartella formicula_cache non esiste nella directory temporanea di Zikula o non è scrivibile - il captcha è stato disabilitato');
+define('_FOR_CANCELDELETE','Cancel deletion routine' );
+define('_FOR_CANNOTCREATEFOLDEROUTSIDEWEBROOT', 'la cartella pnTemp si trova all\'esterno della webroot, per favore leggi il manuale su come creare la cartella formicula_cache in questo caso.'); 
+define('_FOR_CLEARIMAGECACHE', 'Pulisci la cache di captcha' );
+define('_FOR_COMMENT', 'Testo del messaggio' );
+define('_FOR_COMPANY', 'Società' );
+define('_FOR_CONFIRMDELETE','Clicca qui per eliminare questo contatto' );
+define('_FOR_CONTACTCREATED', 'Contatto creato');
+define('_FOR_CONTACTDELETED', 'Il conttatto è stato eliminato');
+define('_FOR_CONTACTFORM', 'Contact Form');
+define('_FOR_CONTACTID','ID' );
+define('_FOR_CONTACTNAME','Nome');
+define('_FOR_CONTACTTITLE', 'Contatta il nostro team' );
+define('_FOR_CONTACTUPDATED', 'Il contatto è stato aggiornato');
+define('_FOR_CREATECONTACTFAILED', 'Errore durante la creazione del contatto!');
+define('_FOR_CREATEFILESFAILED', 'L\'installer non può creare formicula_cache/index.html e/o formicula_cache/.htaccess, per favore controlla il manuale prima di usare il modulo!');
+define('_FOR_CREATEFOLDERFAILED', 'L\'installer non può creare la cartella formicula_cache, per favore controlla il manuale prima di usare il modulo!');
+define('_FOR_CREATETABLEFAILED', 'L\'installer non può creare la tabella dei contatti');
+
+//
+// D
+//
+define('_FOR_DBUPGRADEFAILED', 'Upgrade del database fallito');
+define('_FOR_DELETE','Elimina contatto' );
+define('_FOR_DELETECONTACT','Elimina contatto' );
+define('_FOR_DELETETABLEFAILED', 'impossibile eliminare la tabella');
+define('_FOR_DELETEUPLOADEDFILE','Elimina il file dopo aver spedito' );
+define('_FOR_DESC', 'Strumento per la creazione di contact form di ogni tipo');
+
+//
+// E
+//
+define('_FOR_EDIT','Modifica contatto' );
+define('_FOR_EDITCONFIG','Modifica configurazione' );
+define('_FOR_EDITCONTACT','Modifica contatto' );
+define('_FOR_EMAIL','Email' );
+define('_FOR_EMAILFROM', 'Email da');
+define('_FOR_ERROR', 'C\'è un errore nel tuo form' );
+define('_FOR_ERRORCOMMENT', 'Errore: testo del messaggio invalido o mancante (no HTML!)');
+define('_FOR_ERRORCONTACT', 'Errore: nome mancante');
+define('_FOR_ERRORCREATINGCONTACT', 'Impossibile creare contatto!');
+define('_FOR_ERROREMAIL', 'Errore: indirizzo e-mail invalido o mancante');
+define('_FOR_ERRORINVALIDEMAIL', 'Errore: indirizzo e-mail invalido');
+define('_FOR_ERRORNOMANDATORYFIELD', 'Errore: alcuni campi obbligatori sono vuoti');
+define('_FOR_ERRORSENDINGMAIL', 'Si è verificato un errore durante l\'invio dell\'e-mail.');
+define('_FOR_ERRORSENDINGUSERMAIL', 'Si è verificato un errore interno durante l\'invio dell\'e-mail di conferma.' );
+define('_FOR_ERRORUPLOADERROR', 'Errore: Errore di upload');
+define('_FOR_ERRORUSERNAME', 'Errore: username mancante');
+define('_FOR_EXCLUDEFROMSPAMCHECK', 'Non utilizzare lo spamcheck nei seguenti form <br />(lista degli id dei form separati da una virgola, es. form integrati in pagesetter. Il redirect potrebbe non funzionare correttamente in questo caso)');
+
+//
+// F
+//
+define('_FOR_FORMICULA','Formicula!' );
+define('_FOR_FORMNUMBER', 'Form #' );
+
+//
+// H
+//
+define('_FOR_HELLO', 'Salve,' );
+define('_FOR_HTACCESSPROBLEM', 'Il file .htaccess non esiste nella cartella formicula_cache');
+define('_FOR_HTMLMAIL', 'HTML' );
+
+//
+// I
+//
+define('_FOR_ILLEGALEMAIL', 'rilevato indirizzo e-mail invalido');
+
+//
+// L
+//
+define('_FOR_LOCATION', 'Luogo' );
+
+//
+// M
+//
+define('_FOR_MUSTBE', 'Campo obbligatorio' );
+
+//
+// N
+//
+define('_FOR_NAME', 'Il tuo nome' );
+define('_FOR_NAMEOFCONTACT','Nome del contatto' );
+define('_FOR_NOAUTH', 'Non sei autorizzato a compiere questa azione.');
+define('_FOR_NOAUTHFORFORM', 'Non sei autorizzato ad utilizzare questo form.');
+define('_FOR_NOCONTACTS', 'Nessun contatto trovato.');
+define('_FOR_NOFORMSELECTED', 'nessun form selezionato');
+define('_FOR_NOIMAGEFUNCTION', 'nessuna funzione per immagini disponibile - captcha disattivato');
+define('_FOR_NOMAILERMODULE', 'Il modulo mailer non è disponibile - impossibile inviare e-mail!');
+define('_FOR_NOSUCHCONTACT', 'Contatto sconosciuto');
+
+//
+// O
+//
+define('_FOR_ONLINEAPPLYAS', 'Fai domanda come' );
+define('_FOR_ONLINEBIRTHDATE', 'Data di nascita' );
+define('_FOR_ONLINECOUNTRY', 'Nazione' );
+define('_FOR_ONLINEDATE', 'Data della domanda' );
+define('_FOR_ONLINEJOBAPPLY', 'Fai domanda online!' );
+define('_FOR_ONLINEPRIVACY', 'Grazie per aver fatto domanda, manterremo i tuoi dati strettamente confidenziali' );
+define('_FOR_ONLINESALARY', 'Salario' );
+define('_FOR_ONLINESTREET', 'Indirizzo' );
+define('_FOR_ONLINEZIPCITY', 'Zip Città' );
+define('_FOR_OPTIONS','Opzioni' );
+
+//
+// P
+//
+define('_FOR_PHONE', 'Numero di telefono' );
+define('_FOR_PUBLIC', 'Pubblico' );
+
+//
+// R
+//
+define('_FOR_RESUME','Riprendi');
+
+//
+// S
+//
+define('_FOR_SEND', 'Invia' );
+define('_FOR_SENDEREMAIL', 'E-mail del mittente');
+define('_FOR_SENDERINFO', 'Queste informazioni saranno usate nell\'e-mail di conferma');
+define('_FOR_SENDERNAME', 'Nome del mittente');
+define('_FOR_SENDERSUBJECT', 'Oggetto');
+define('_FOR_SENDERSUBJECTHINT', '
+con <ul>
+    <li>%s = nome del sito</li>
+    <li>%l = slogan</li>
+    <li>%u = indirizzo del sito</li>
+    <li>%c = nome del mittente</li>
+    <li>%n&lt;num&gt; = nome del campo definito dall\'utente &lt;num&gt;</li>
+    <li>%d&lt;num&gt; = valore del campo definito dall\'utente &lt;num&gt;</li>
+</ul>
+');
+define('_FOR_SENDTOADMIN', 'Ci sono state inviate le seguenti informazioni:');
+define('_FOR_SENDTOUSER', 'La conferma della tua iscrizione ti verrà inviata entro pochi minuti.' );
+define('_FOR_SENDUSER','Invia un\'e-mail di conferma all\'utente?' );
+define('_FOR_SHOWCOMMENT', 'Chiedi il testo del messaggio?' );
+define('_FOR_SHOWCOMPANY','Chiedi il nome della società?' );
+define('_FOR_SHOWLOCATION','Chiedi il luogo?' );
+define('_FOR_SHOWPHONE','Chiedi il numero di telefono?' );
+define('_FOR_SHOWURL','Chiedi l\'URL?' );
+define('_FOR_SIMPLEMATHEQUATION', 'Per favore risolvi questo semplice calcolo matematico');
+define('_FOR_SUBMIT', 'Aggiorna la configurazione' );
+
+//
+// T
+//
+define('_FOR_TEAM', 'Team' );
+define('_FOR_TEXTMAIL', 'Testo' );
+define('_FOR_THANKS', 'Grazie per le tue domande/commenti al nostro sito!<br />Ti risponderemo appena possibile.' );
+define('_FOR_THE', 'Il' );
+define('_FOR_THEME', 'Contatto o Argomento' );
+
+//
+// U
+//
+define('_FOR_UPDATECONTACTFAILED', 'Errore durante l\'aggiornamento del contatto!');
+define('_FOR_UPLOADDIRNOTWRITABLE','Il webserver non può scrivere questa cartella!' );
+define('_FOR_UPLOADERROR1', 'upload-error: file troppo grosso (php.ini)' );
+define('_FOR_UPLOADERROR2', 'upload-error: file troppo grosso (form)' );
+define('_FOR_UPLOADERROR3', 'upload-error: file ricevuto solo parzialmente' );
+define('_FOR_UPLOADERROR4', 'upload-error: nessun file ricevuto' );
+define('_FOR_UPLOADFILEDIR', 'Cartella per i file uploadati' );
+define('_FOR_UPLOADLIMIT', '(Upload, max. 2MB)');
+define('_FOR_URL', 'Homepage' );
+define('_FOR_USERMAIL1', 'Grazie per i commenti sul nostro sito. Le informazioni inviate sono:' );
+define('_FOR_USERMAIL2', 'Risponderemo alla tua e-mail appena possibile.' );
+define('_FOR_USERMAILFORMAT', 'Formato dell\'e-mail' );
+
+//
+// V
+//
+define('_FOR_VIEWCONTACT','Elenco contatti' );
+define('_FOR_VISITHOMEPAGE', 'Visita il Trac di Formicula');
+
+//
+// W
+//
+define('_FOR_WRONGCAPTCHA', 'Problemi con la matematica? Puoi far di meglio, riprova.');
---
+++ formicula/pnlang/ita/version.php
@@ -0,0 +1,24 @@
+<?php
+// $Id$
+// ----------------------------------------------------------------------
+// LICENSE
+//
+// This program is free software; you can redistribute it and/or
+// modify it under the terms of the GNU General Public License (GPL)
+// as published by the Free Software Foundation; either version 2
+// of the License, or (at your option) any later version.
+//
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// To read the license please visit http://www.gnu.org/copyleft/gpl.html
+// ----------------------------------------------------------------------
+/**
+ * @copyright (C) Frank Schummertz
+ * @link http://code.zikula.org/projects/community-italian/
+ * @translated by Arthens
+ */
+
+define('_FOR_DESC', 'Strumento per creare contact form di ogni tipo');
