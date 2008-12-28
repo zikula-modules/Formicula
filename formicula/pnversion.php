0ecc88fcfdabc2a73d448043db52beeff56f0c0c
Landseer <Landseer@2cecb711-b51b-0410-93e8-fb3f36be984d>
2008-12-28 13:34:07 +0000

    coding guideline change
    git-svn-id: https://code.zikula.org/svn/formicula/trunk@131 2cecb711-b51b-0410-93e8-fb3f36be984d

--- formicula/pnversion.php
+++ formicula/pnversion.php
@@ -1,24 +1,17 @@
 <?php
-// $Id$
-// ----------------------------------------------------------------------
-// LICENSE
-//
-// This program is free software; you can redistribute it and/or
-// modify it under the terms of the GNU General Public License (GPL)
-// as published by the Free Software Foundation; either version 2
-// of the License, or (at your option) any later version.
-//
-// This program is distributed in the hope that it will be useful,
-// but WITHOUT ANY WARRANTY; without even the implied warranty of
-// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-// GNU General Public License for more details.
-//
-// To read the license please visit http://www.gnu.org/copyleft/gpl.html
-// ----------------------------------------------------------------------
-// Original Author of file: Frank Schummertz
-// Purpose of file:  formicula version information
-// ----------------------------------------------------------------------
-
+/**
+ * Formicula - the contact mailer for Zikula
+ * -----------------------------------------
+ *
+ * @copyright  (c) Formicula Development Team
+ * @link       http://code.zikula.org/formicula 
+ * @version    $Id$
+ * @license    GNU/GPL - http://www.gnu.org/copyleft/gpl.html
+ * @author     Frank Schummertz <frank@zikula.org>
+ * @package    Third_Party_Components
+ * @subpackage formicula
+ */
+ 
 $modversion['name'] = 'formicula';
 $modversion['version'] = '2.1';
 $modversion['description'] = _FOR_DESC;
@@ -28,7 +21,7 @@ $modversion['changelog'] = 'pndocs/changelog.txt';
 $modversion['license'] = 'pndocs/license.txt';
 $modversion['official'] = 0;
 $modversion['author'] = 'Frank Schummertz';
-$modversion['contact'] = 'frank.schummertz@landseer-stuttgart.de';
+$modversion['contact'] = 'frank@zikula.org';
 $modversion['admin'] = 1;
 $modversion['user'] = 1;
 $modversion['securityschema'] = array('formicula::' => 'form_id:contact_id:' );
