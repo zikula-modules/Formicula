8de5b6370e8fbfc11398831d6290fc310a556c6e
herr.vorragend <herr.vorragend@2cecb711-b51b-0410-93e8-fb3f36be984d>
2009-09-26 10:43:25 +0000

    changed name of css-class
    git-svn-id: https://code.zikula.org/svn/formicula/trunk@154 2cecb711-b51b-0410-93e8-fb3f36be984d

--- formicula/pntemplates/plugins/function.cssversion.php
+++ formicula/pntemplates/plugins/function.cssversion.php
@@ -19,7 +19,7 @@ function smarty_function_cssversion($params, &$smarty)
     $assign = isset($params['assign']) ? $params['assign'] : null;
 
     if (version_compare(PN_VERSION_NUM, '1.2.0-dev', '>=')) {
-        $return = 'zk-form';
+        $return = 'z-form';
     } else {
         $return = 'pn-adminform';
     }
