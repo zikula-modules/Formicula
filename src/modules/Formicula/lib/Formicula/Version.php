1935147b02d044339a9cf21323c969b6b1d16e49
Erik Spaan <erik.spaan@gmail.com>
2012-12-07 13:36:46 +0100

    Set core requirement back to 1.3 for version 3.0.1
    Master branch will be the development branch, no specific 3.0 branch

--- src/modules/Formicula/lib/Formicula/Version.php
+++ src/modules/Formicula/lib/Formicula/Version.php
@@ -16,9 +16,9 @@ class Formicula_Version extends Zikula_AbstractVersion
     public function getMetaData()
     {
         $meta = array();
-        $meta['version'] = '3.1.0';
-        $meta['core_min'] = '1.4.0'; // Fixed to 1.3.x range
-        $meta['core_max'] = '1.4.99'; // Fixed to 1.3.x range
+        $meta['version'] = '3.0.1';
+        $meta['core_min'] = '1.3.0'; // Fixed to 1.3.x range
+        $meta['core_max'] = '1.3.99'; // Fixed to 1.3.x range
         $meta['oldnames']    = array('formicula');
         $meta['description'] = $this->__('Formicula forms module');
         $meta['displayname'] = $this->__('Formicula');
